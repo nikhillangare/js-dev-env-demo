c1790a3c3d02ac0f1584777a60cf6e5533984beb
Nikhil Langare <nikhillangare@Nikhils-MacBook-Pro.local>
2018-06-05 16:39:04 -0700

    final to deploy

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -14,9 +16,13 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //Gen external css with hash filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    //Hash files to change names on content change
+    new WebpackMd5Hash(),
     //CommonsChunkPlugin to create separate bundle for vendor libraries
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
@@ -35,7 +41,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
